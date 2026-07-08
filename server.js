87cbdf40cdf32748f18a3de909d6b9ffbfb20929
sanjaynesan-05 <sanjaynesan007@gmail.com>
2026-07-08 09:46:13 +0530

    initialization

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./db/db");
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => { 
-    res.send("server is on");
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
-});
+})
